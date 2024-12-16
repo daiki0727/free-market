88ea12223d7cafe92fbce3ac5ea879db9360c963
daiki hayashi <daiki.pote@gmail.com>
2024-12-16 22:50:57 +0900

    出品画面の作成

--- src/resources/views/listing.blade.php
+++ src/resources/views/listing.blade.php
@@ -10,6 +10,7 @@
     <form class="listing__form" action="/item-listing" method="POST" enctype="multipart/form-data">
         @csrf
 
+        <label class="item-image__label">商品画像</label>
         <div class="item-image-box">
             <!-- 画像選択ボタン -->
             <label class="item-picture__btn" for="item-picture__input">
@@ -21,41 +22,53 @@
         </div>
 
         <!-- 商品の詳細 -->
+        <h2 class="item-detail__heading">商品の詳細</h2>
         <div class="item-detail__box">
-            <label class="categories__label">カテゴリー</label>
-            <input class="categories__input" type="text" name="categories" value="">
-            @error('categories')
-                <div class="error">{{ $message }}</div>
-            @enderror
-            <label class="condition__label">商品の状態</label>
-            <input class="condition__input" type="text" name="conditions" value="">
-            @error('conditions')
-                <div class="error">{{ $message }}</div>
-            @enderror
+            <div class="categories__area">
+                <label class="categories__label">カテゴリー</label>
+                <input class="categories__input" type="text" name="categories" value="">
+                @error('categories')
+                    <div class="error">{{ $message }}</div>
+                @enderror
+            </div>
+            <div class="condition__area">
+                <label class="condition__label">商品の状態</label>
+                <input class="condition__input" type="text" name="conditions" value="">
+                @error('conditions')
+                    <div class="error">{{ $message }}</div>
+                @enderror
+            </div>
         </div>
 
         <!-- 商品名と説明 -->
+        <h2 class="item-description__heading">商品名と説明</h2>
         <div class="item-description__box">
-            <label class="item-name__label">商品名</label>
-            <input class="item-name__input" type="text" name="item_name" value="">
-            @error('item_name')
-                <div class="error">{{ $message }}</div>
-            @enderror
-            <label class="item-description__label">商品の説明</label>
-            <input class="item-description__input" type="text" name="description" value="">
-            @error('description')
-                <div class="error">{{ $message }}</div>
-            @enderror
+            <div class="item-name__area">
+                <label class="item-name__label">商品名</label>
+                <input class="item-name__input" type="text" name="item_name" value="">
+                @error('item_name')
+                    <div class="error">{{ $message }}</div>
+                @enderror
+            </div>
+            <div class="item-description__area">
+                <label class="item-description__label">商品の説明</label>
+                <textarea class="item-description__textarea" type="text" name="description" value=""></textarea>
+                @error('description')
+                    <div class="error">{{ $message }}</div>
+                @enderror
+            </div>
         </div>
 
         <!-- 販売価格 -->
+        <h2 class="price__heading">販売価格</h2>
         <div class="price__box">
-            <label class="price__label">販売価格</label>
-            <input class="price__input" type="text" name="price"
-                value="">
-            @error('price')
-                <div class="error">{{ $message }}</div>
-            @enderror
+            <div class="price__area">
+                <label class="price__label">販売価格</label>
+                <input class="price__input" type="text" name="price" value="" placeholder="¥">
+                @error('price')
+                    <div class="error">{{ $message }}</div>
+                @enderror
+            </div>
         </div>
 
         <!-- 出品ボタン -->
